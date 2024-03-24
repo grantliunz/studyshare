b3cf6a8098d6b3d66a41a443213973ec8ee9b330
Grant Liu <grant.liux@gmail.com>
2024-03-24 15:46:06 +1300

    feat: created landing and login page

---
+++ frontend/src/pages/landing/Landing.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+export default function Landing() {
+    return (
+        <div>
+            <h1>Landing Page</h1>
+            <Button variant="contained" color="primary" href="/login">
+                Login
+            </Button>
+        </div>
+    );
+}
---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,61 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+export default function Login() {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get("email") as string;
+        const password = data.get("password") as string;
+        console.log({ email, password });
+    };
+
+    return (
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+            />
+
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Sign In
+            </Button>
+            <Grid container>
+                <Grid item xs>
+                    <Link href="#" variant="body2">
+                        Forgot password?
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Link href="#" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                    </Link>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
