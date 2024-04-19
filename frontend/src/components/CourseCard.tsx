5cc396f107c368ebadaf49253b90212d6042993c
cste303 <100255176+cste303@users.noreply.github.com>
2024-04-19 23:19:34 +1200

    feat: Course Page
    created base layout for course page with hardcoded data

--- frontend/src/components/CourseCard.tsx
+++ frontend/src/components/CourseCard.tsx
@@ -1,19 +1,44 @@
-import style from "./CourseCard.module.css";
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material';
+import style from './CourseCard.module.css';
 
 interface CourseCardProps {
-  courseName: string,
-  courseCode: string,
+  courseName: string;
+  courseCode: string;
+  onClick: () => void;
 }
 
-const CourseCard: React.FC<CourseCardProps> = ({ courseName, courseCode }) => {
+const CourseCard: React.FC<CourseCardProps> = ({
+  courseName,
+  courseCode,
+  onClick
+}) => {
   return (
-    <div className={style.courseCard}>
-      <p className={style.courseCode}>{courseCode}</p>
-      <p className={style.courseName}>{courseName}</p>
-    </div>
+    <Card
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#d9d9d9',
+        borderRadius: 5,
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.4)',
+        minWidth: 800,
+        cursor: 'pointer',
+        outline: 'none',
+        maxHeight: 70,
+        marginBottom: 1
+      }}
+      onClick={onClick}
+    >
+      <CardActionArea>
+        <CardContent className={style.cardContent}>
+          <p className={style.courseCode}>{courseCode}</p>
+          <p className={style.courseName}>{courseName}</p>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
-}
+};
 
 export default CourseCard;
-
-
--- frontend/src/pages/Course/Course.tsx
+++ frontend/src/pages/Course/Course.tsx
@@ -1,15 +1,66 @@
 import CourseCard from '../../components/CourseCard';
 import SearchBar from '../../components/SearchBar';
 
+const courseData = [
+  { courseCode: 'SOFTENG 284', courseName: 'Data Structures and Algorithms' },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  },
+  { courseCode: 'SOFTENG 206', courseName: 'Software Engineering' },
+  {
+    courseCode: 'SOFTENG 251',
+    courseName: 'Introduction to Software Engineering'
+  }
+];
+
 export default function CoursePage() {
   return (
     <div>
       <h1>Course Page</h1>
 
       <SearchBar title="Search for a course" />
-      <br/>
+      <br />
 
-      <CourseCard courseCode='SOFTENG 284' courseName='Data Structures and Algorithms'/>
+      {courseData.map((course, index) => (
+        <CourseCard
+          key={index}
+          courseCode={course.courseCode}
+          courseName={course.courseName}
+          onClick={() => console.log('clicked')}
+        />
+      ))}
     </div>
   );
 }
