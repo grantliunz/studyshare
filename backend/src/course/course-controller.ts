0c301043870cf55304f0a3ce1076f70828abd3df
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2024-04-23 00:40:12 +1200

    feat: Added routes for courses

--- backend/src/course/course-controller.ts
+++ backend/src/course/course-controller.ts
@@ -1,6 +1,137 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { CourseDTO } from "./course-dto";
+import Course from "./course-model";
+import University from "../university/university-model";
 
-// Dummy route
-export const dummyTest = async (req: Request, res: Response) => {
-  return res.send("Hello, World");
+// controller function to get all courses
+export const getAllCourses = async (req: Request, res: Response) => {
+  try {
+    // fetch all courses from the database
+    const courses = await Course.find();
+
+    res.status(200).json(courses); // respond with all courses
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+// controller function to get all courses in a university
+export const getAllCoursesInUniversity = async (req: Request, res: Response) => {
+  try {
+    // get the university by its ID
+    const university = await University.findById(req.params.universityId);
+
+    if (!university) {
+      return res.status(404).json({ message: "University not found" });
+    }
+
+    // fetch all courses in the university
+    const courses = await Course.find({ _id: { $in: university.Courses } });
+
+    res.status(200).json(courses); // respond with all courses in the university
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+// controller function to get a course by its ID
+export const getCourse = async (req: Request, res: Response) => {
+  try {
+    // fetch the course by its ID from the database
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    res.status(200).json(course); // respond with the course
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+// controller function to create a new course
+export const createCourse = async (req: Request<{ universityId: string }, {}, CourseDTO>, res: Response) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { Name } = req.body; // assuming request body contains course data
+    
+    // Get the university by its ID
+    const university = await University.findById(req.params.universityId);
+
+    if (!university) {
+      return res.status(404).json({ message: "University not found" });
+    }
+
+    // check if course with the same name already exists
+    const existingCourse = await Course.findOne({ Name, University: req.params.universityId });
+
+    if (existingCourse) {
+      return res.status(400).json({ error: "Course with the same name already exists in this university" });
+    }
+
+    // create a new course instance and add code and assessments if available
+    const course = new Course({ Name });
+
+    // save the course to the database
+    const createdCourse = await course.save();
+
+    // add the course ID to the university's courses array
+    university.Courses.push(createdCourse._id);
+
+    // save the university with the updated courses array
+    await university.save();
+
+    res.status(201).json(createdCourse); // respond with the created course
+  } catch (error) {
+    res.status(500).json({ message: `Internal server error: ${error}` });
+  }
+};
+
+
+// controller function to update a course by its ID
+export const updateCourse = async (req: Request<{ id: string }, {}, CourseDTO>, res: Response) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { Name } = req.body; // assuming request body contains course data
+
+    // find the course by its ID and update it
+    const updatedCourse = await Course.findByIdAndUpdate(req.params.id, { Name }, { new: true });
+
+    if (!updatedCourse) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    res.status(200).json(updatedCourse); // respond with the updated course
+
+  } catch (error) {
+    res.status(500).json({ message: `Internal server error: ${error}` });
+  }
+};
+
+
+// controller function to delete a course by its ID
+export const deleteCourse = async (req: Request, res: Response) => {
+  try {
+    // delete the course by its ID
+    const deletedCourse = await Course.findByIdAndDelete(req.params.id);
+
+    if (!deletedCourse) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    return res.status(200).json(deletedCourse); // respond with the deleted course
+  } catch (error) {
+    return res.status(500).json({ message: `Internal server error: ${error}` });
+  }
 };
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -7,12 +7,14 @@ import userRouter from "../user/user-route";
 import questionRouter from "../question/question-route";
 import answerRouter from "../answer/answer-route";
 import universityRouter from "../university/university-route";
+import courseRouter from "../course/course-route";
 
 // Use routers
 router.use("/user", userRouter);
 router.use("/question", questionRouter);
 router.use("/answer", answerRouter);
 router.use("/university", universityRouter);
+router.use("/course", courseRouter);
 
 
 export default router;
