edb0a4976db2ed88a218229ac11c9f8341d70609
Aaron Rodrigues <ardo715@aucklanduni.ac.nz>
2024-04-29 16:33:26 +1200

    fix: Fixed duplicate courses being added

--- backend/src/course/course-dto.ts
+++ backend/src/course/course-dto.ts
@@ -1,9 +1,9 @@
-import { Types } from "mongoose";
-import { CreateAssessmentDTO } from "../assessment/assessment-dto";
+import { Types } from 'mongoose';
+import { CreateAssessmentDTO } from '../assessment/assessment-dto';
 
 export interface CourseDTO {
-    Name: string;
-    Code?: string;
-    Assessments?: CreateAssessmentDTO[] | Types.ObjectId[];
-  }
-  
+  Name: string;
+  Code?: string;
+  University: string;
+  Assessments?: CreateAssessmentDTO[] | Types.ObjectId[];
+}
--- backend/src/course/course-model.ts
+++ backend/src/course/course-model.ts
@@ -1,28 +1,35 @@
 import mongoose, { Model, Schema, Types } from 'mongoose';
 
 export interface ICourse extends Document {
-    Name: string;
-    Code: string;
-    Assessments: Types.ObjectId[];
+  Name: string;
+  Code: string;
+  University: string;
+  Assessments: Types.ObjectId[];
 }
 
-
-const courseSchema: Schema<ICourse> = new Schema({
+const courseSchema: Schema<ICourse> = new Schema(
+  {
     Name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     Code: {
-        type: String,
+      type: String
+    },
+    University: {
+      type: String,
+      required: true
     },
     Assessments: {
-        type: [Schema.Types.ObjectId],
-        ref: 'Assessment',
-        default: []
-    },
-}, {
+      type: [Schema.Types.ObjectId],
+      ref: 'Assessment',
+      default: []
+    }
+  },
+  {
     timestamps: {}
-});
+  }
+);
 
 const Course: Model<ICourse> = mongoose.model('Course', courseSchema);
 export default Course;
