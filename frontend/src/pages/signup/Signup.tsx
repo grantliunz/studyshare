4f2e0b191555fd5fc7d980a1767fa6b5af33a194
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2024-05-02 16:33:24 +1200

    fix: focus set to first name

--- frontend/src/pages/signup/Signup.tsx
+++ frontend/src/pages/signup/Signup.tsx
@@ -128,7 +128,6 @@ export default function Signup() {
           label="Email Address"
           name="email"
           autoComplete="email"
-          autoFocus
         />
         <TextField
           margin="normal"
