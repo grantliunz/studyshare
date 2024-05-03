9318df99fd750aa1080d84171007b821afc1682d
Leander Legaspi <101154372+LeanderUoa@users.noreply.github.com>
2024-05-03 16:06:47 +1200

    changed tabbing

--- frontend/src/components/LoginPopup/LoginPopup.tsx
+++ frontend/src/components/LoginPopup/LoginPopup.tsx
@@ -11,113 +11,114 @@ import { useEffect, useState } from 'react';
 import { getFirebaseErrorMessage } from '../../util/firebase';
 
 interface LoginPopupProps {
-    open: boolean;
-    setOpen: (open: boolean) => void;
+  open: boolean;
+  setOpen: (open: boolean) => void;
 }
 
 export default function LoginPopup({ open, setOpen }: LoginPopupProps) {
 
-    const [notice, setNotice] = useState('');
-    const [loading, setLoading] = useState(false); // State to track loading state
-    const { user, login, loginWithGoogle } = useAuth();
+  const [notice, setNotice] = useState('');
+  const [loading, setLoading] = useState(false); // State to track loading state
+  const { user, login, loginWithGoogle } = useAuth();
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setLoading(true); // Set loading state to true
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true); // Set loading state to true
 
-        const data = new FormData(event.currentTarget);
-        const email = data.get('email') as string;
-        const password = data.get('password') as string;
-        login(email, password)
-        .then(() => {
-            setOpen(false);
-            setLoading(false); // Set loading state to false on success
-        })
-        .catch((error: FirebaseError) => {
-            setNotice(getFirebaseErrorMessage(error.code));
-            setLoading(false); // Set loading state to false on error
-        });
-
-        // Create a new
-    };
-
-    const submitGoogle = async () => {
-        await loginWithGoogle();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email') as string;
+    const password = data.get('password') as string;
+    login(email, password)
+      .then(() => {
         setOpen(false);
-    };
+        setLoading(false); // Set loading state to false on success
+      })
+      .catch((error: FirebaseError) => {
+        setNotice(getFirebaseErrorMessage(error.code));
+        setLoading(false); // Set loading state to false on error
+      });
 
-    return (
-        <Modal
-            open={open}
-            onClose={() => setOpen(false)}
-        >   
-            <div className={styles.modal}>
-                <Paper
-                    component="form"
-                    onSubmit={handleSubmit}
-                    noValidate
-                    style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    margin: '0 auto',
-                    backgroundColor: 'white',
-                    padding: '20px',
-                    alignItems: 'center'
-                    }}
-                >
-                    <h1 className={styles.logo}>
-                    StudyShare
-                    </h1>
-                    <h2 className={styles.title}>Log in</h2>
-                    <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    />
-                    <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    />
+    // Create a new
+  };
 
-                    <p>{notice}</p>
+  const submitGoogle = async () => {
+    await loginWithGoogle();
+    setOpen(false);
+  };
 
-                    <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="secondary"
-                    disabled={loading}
-                    endIcon={loading && <CircularProgress size={20} />}
-                    style={{
-                        marginTop: '20px',
-                        padding: '10px 50px'
-                    }}
-                    >
-                    {!loading ? 'Log in' : ''}
-                    </Button>
-                    <div className={styles.linksWrapper}>
-                    <div>
-                        Don't have an account?&nbsp;
-                        <Link href="/signup">Sign up</Link>
-                    </div>
-                    <Link href="#">Forgot password?</Link>
-                    </div>
-                    <Button onClick={submitGoogle}>Log In with Google</Button>
-                </Paper>
+
+  return (
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+    >
+      <div className={styles.modal}>
+        <Paper
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            margin: '0 auto',
+            backgroundColor: 'white',
+            padding: '20px',
+            alignItems: 'center'
+          }}
+        >
+          <h1 className={styles.logo}>
+            StudyShare
+          </h1>
+          <h2 className={styles.title}>Log in</h2>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <p>{notice}</p>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            disabled={loading}
+            endIcon={loading && <CircularProgress size={20} />}
+            style={{
+              marginTop: '20px',
+              padding: '10px 50px'
+            }}
+          >
+            {!loading ? 'Log in' : ''}
+          </Button>
+          <div className={styles.linksWrapper}>
+            <div>
+              Don't have an account?&nbsp;
+              <Link href="/signup">Sign up</Link>
             </div>
-                
-        </Modal>
-    )
+            <Link href="#">Forgot password?</Link>
+          </div>
+          <Button onClick={submitGoogle}>Log In with Google</Button>
+        </Paper>
+      </div>
+
+    </Modal>
+  )
 }
