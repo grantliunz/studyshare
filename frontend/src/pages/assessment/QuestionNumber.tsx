ff685b5a44f3c39786d6f018c23f906fa6885ced
Grant Liu <grant.liux@gmail.com>
2024-05-03 17:48:54 +1200

    feat: show add question icon only on hover

--- frontend/src/pages/assessment/QuestionNumber.tsx
+++ frontend/src/pages/assessment/QuestionNumber.tsx
@@ -23,9 +23,9 @@ const QuestionNumber = ({
 }: QuestionNumberProps) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
   const [hideText, setHideText] = useState<boolean>(false);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
 
   useEffect(() => {
-    console.log('hmm');
     const width = document.getElementById('questionButton')?.clientWidth;
     if (width && width < 100) {
       setHideText(true);
@@ -36,128 +36,127 @@ const QuestionNumber = ({
 
   return (
     <div>
-      {
-        <>
-          <div
-            style={{
-              alignItems: 'center',
-              columnGap: '4px',
-              display: 'flex',
-              flexDirection: 'row',
-              width: '100%'
-            }}
-          >
-            <IconButton
-              onClick={() => setIsExpanded(!isExpanded)}
-              style={{
-                padding: '3px',
-                visibility: questionNode.subquestions ? 'inherit' : 'hidden'
-              }}
-            >
-              {isExpanded ? (
-                <ExpandMoreRoundedIcon />
-              ) : (
-                <KeyboardArrowRightRoundedIcon />
-              )}
-            </IconButton>
-            <Button
-              onClick={() =>
-                questionNode.question && setQuestion(questionNode.question)
-              }
-              id="questionButton"
-              fullWidth
-              sx={{
-                '&.MuiButtonBase-root:hover': {
-                  bgcolor: questionNode.question
-                    ? arrayEquals(
-                        currentQuestion?.number || [],
-                        questionNode.number
-                      )
-                      ? '#41709b'
-                      : 'rgba(0, 0, 0, 0.04)'
-                    : 'transparent'
-                },
-                '&.MuiButtonBase-root': {
-                  bgcolor: arrayEquals(
+      <div
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%'
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <IconButton
+          onClick={() => setIsExpanded(!isExpanded)}
+          style={{
+            padding: '3px',
+            visibility: questionNode.subquestions ? 'inherit' : 'hidden'
+          }}
+        >
+          {isExpanded ? (
+            <ExpandMoreRoundedIcon />
+          ) : (
+            <KeyboardArrowRightRoundedIcon />
+          )}
+        </IconButton>
+        <Button
+          onClick={() =>
+            questionNode.question && setQuestion(questionNode.question)
+          }
+          id="questionButton"
+          fullWidth
+          sx={{
+            '&.MuiButtonBase-root:hover': {
+              bgcolor: questionNode.question
+                ? arrayEquals(
                     currentQuestion?.number || [],
                     questionNode.number
                   )
-                    ? '#41709b'
-                    : '#E8E9EC'
+                  ? '#41709b'
+                  : 'rgba(0, 0, 0, 0.04)'
+                : 'transparent'
+            },
+            '&.MuiButtonBase-root': {
+              bgcolor: arrayEquals(
+                currentQuestion?.number || [],
+                questionNode.number
+              )
+                ? '#41709b'
+                : '#E8E9EC'
+            }
+          }}
+          style={
+            questionNode.question
+              ? {
+                  border: '1px solid black',
+                  color: arrayEquals(
+                    currentQuestion?.number || [],
+                    questionNode.number
+                  )
+                    ? 'white'
+                    : 'black',
+                  minWidth: '30px',
+                  padding: '0 0 0 10px',
+                  textTransform: 'none',
+                  display: 'flex',
+                  justifyContent: 'start',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden'
                 }
-              }}
-              style={
-                questionNode.question
-                  ? {
-                      border: '1px solid black',
-                      color: arrayEquals(
-                        currentQuestion?.number || [],
-                        questionNode.number
-                      )
-                        ? 'white'
-                        : 'black',
-                      minWidth: '30px',
-                      padding: '0 0 0 10px',
-                      textTransform: 'none',
-                      display: 'flex',
-                      justifyContent: 'start',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden'
-                    }
-                  : {
-                      textTransform: 'none',
-                      minWidth: '30px',
-                      padding: '0 0 0 0',
-                      pointerEvents: 'none',
-                      display: 'flex',
-                      justifyContent: 'start'
-                    }
-              }
-            >
-              {questionNode.number.at(-1)}&nbsp;
-              <span
-                style={{
-                  opacity: 0.5,
-                  fontSize: '0.8rem',
-                  display: hideText ? 'none' : 'inline'
-                }}
-              >
-                {questionNode.question?.text
-                  .replace(/<\/?[^>]+(>|$)/g, '')
-                  .trim()}
-              </span>
-            </Button>
-            <IconButton
-              style={{ padding: '4px', marginLeft: 'auto' }}
-              onClick={() => handleAddQuestion(questionNode.number)}
-            >
-              <AddIcon fontSize="small" />
-            </IconButton>
-          </div>
-          <div
+              : {
+                  textTransform: 'none',
+                  minWidth: '30px',
+                  padding: '0 0 0 0',
+                  pointerEvents: 'none',
+                  display: 'flex',
+                  justifyContent: 'start'
+                }
+          }
+        >
+          {questionNode.number.at(-1)}&nbsp;
+          <span
             style={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: isExpanded ? 'auto' : '0px',
-              paddingLeft: '12px',
-              rowGap: '4px',
-              visibility: isExpanded ? 'inherit' : 'hidden'
+              opacity: 0.5,
+              fontSize: '0.8rem',
+              display: hideText ? 'none' : 'inline'
             }}
           >
-            {questionNode.subquestions &&
-              questionNode.subquestions.map((q) => (
-                <QuestionNumber
-                  key={q.number.join(',')}
-                  questionNode={q}
-                  setQuestion={setQuestion}
-                  currentQuestion={currentQuestion}
-                  handleAddQuestion={handleAddQuestion}
-                />
-              ))}
-          </div>
-        </>
-      }
+            {questionNode.question?.text.replace(/<\/?[^>]+(>|$)/g, '').trim()}
+          </span>
+        </Button>
+        <IconButton
+          style={{
+            padding: '4px',
+            margin: '0 10px 0 0',
+            visibility: isHovered ? 'inherit' : 'hidden'
+          }}
+          onClick={() => handleAddQuestion(questionNode.number)}
+        >
+          <AddIcon fontSize="small" />
+        </IconButton>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: isExpanded ? 'auto' : '0px',
+          paddingLeft: '12px',
+          rowGap: '4px',
+          visibility: isExpanded ? 'inherit' : 'hidden'
+        }}
+      >
+        {questionNode.subquestions &&
+          questionNode.subquestions.map((q) => (
+            <QuestionNumber
+              key={q.number.join(',')}
+              questionNode={q}
+              setQuestion={setQuestion}
+              currentQuestion={currentQuestion}
+              handleAddQuestion={handleAddQuestion}
+            />
+          ))}
+      </div>
     </div>
   );
 };
