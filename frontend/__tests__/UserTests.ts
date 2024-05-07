abb22a897cbfb8115789be24063e34163e1025e2
Leander Legaspi <101154372+LeanderUoa@users.noreply.github.com>
2024-05-07 23:09:23 +1200

    now auto generates test ids

--- frontend/__tests__/UserTests.ts
+++ frontend/__tests__/UserTests.ts
@@ -2,11 +2,32 @@
 
 import API from '../src/util/api'
 import { getResponse, postResponse } from '../testUtil/testUtil';
-import { userId} from '../testUtil/testIds';
+import { userId } from '../testUtil/testIds';
+import { CreateUserDTO } from '../../backend/src/routes/user/user-dto'
+
+const randomUserNo = Math.floor(Math.random() * 1000);
 
 it('getUser', async () => { 
  expect (await getResponse(`${API.getUser}/`+ userId)).toBe(200);
 });
+
+it('createUser' , async () => {
+
+ const user: CreateUserDTO = {
+  authId: randomUserNo,
+  name: 'your_name',
+  email: 'your_email' + randomUserNo + '@gmail.com',
+  questions: [],
+  answers: [],
+  watchList: [],
+  reported: [],
+  upvotedAnswers: [],
+  downvotedAnswers: [],
+  upvotedComments: [],
+  downvotedComments: []
+ };
+ expect (await postResponse(`${API.createUser}`, user)).toBe(201);
+ });
 
 it('getNonexistentUser', async () => { 
  expect (await getResponse(`${API.getUser}/`+ '0')).toBe(404);
