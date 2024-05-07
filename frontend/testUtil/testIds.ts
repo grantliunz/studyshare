7519842007ab7004ee5cca91f42d59ae4cfe937c
Leander Legaspi <101154372+LeanderUoa@users.noreply.github.com>
2024-05-07 15:04:30 +1200

    answer and comment tests

---
+++ frontend/testUtil/testIds.ts
@@ -0,0 +1,7 @@
+export const userId = '663985f40f7958b9afd1304c'
+export const answerId = '663995b5459223cef8c1787f'
+export const questionId = '663985f40f7958b9afd1305b'
+export const universityId = '662f9cf0c02c023a03c89544'
+export const courseId = '663985f40f7958b9afd13051'
+export const assessmentId = '663985f40f7958b9afd13057'
+export const commentId = '6639960a459223cef8c17917'
